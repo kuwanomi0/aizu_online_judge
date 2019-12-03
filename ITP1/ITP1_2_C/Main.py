63cde4bd7735686007d1ffa0de54c80c0f93f6ad
NSW00_009102 <kwatana20@gw.nsw.co.jp>
2019-12-03 16:51:59 +0900

    ITP1_2_C Complete

---
+++ ITP1/ITP1_2_C/Main.py
@@ -0,0 +1,7 @@
+# ITP1_2_C
+# Sorting 
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_2_B
+# encode : utf-8
+numberList = list(map(int, input().split()))
+numberList.sort()
+print("{0[0]} {0[1]} {0[2]}".format(numberList))
