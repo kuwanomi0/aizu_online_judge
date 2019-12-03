30c760776c8b91835b51c775d48eb37a0ba09368
NSW00_009102 <kwatana20@gw.nsw.co.jp>
2019-12-03 16:43:18 +0900

    ITP1_2_B Complete

---
+++ ITP1/ITP1_2_B/Main.py
@@ -0,0 +1,9 @@
+# ITP1_2_B
+# Range
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_2_B
+# encode : utf-8
+a, b, c = map(int, input().split())
+if a < b < c :
+    print('Yes')
+else :
+    print('No')
